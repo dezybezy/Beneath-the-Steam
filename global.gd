3524d115dd5c07f285bea732f84d9a9d3ae0c1e8
dezybezy <dezyabreu26@gmail.com>
2026-07-22 08:11:06 +0900

    beh

---
+++ global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var current_scene = "world";
+var transition_scene = false;
+
+var player_exit_scene1_posx =0;
+var player_exit_scene1_posy =0;
+var player_start_posx =0;
+var player_start_posy =0;
